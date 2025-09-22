7ca2e0029bc71b00351a0d2228b6577f150bfdee
Ainhize Arrese <144795043+arreseAinhize@users.noreply.github.com>
2025-09-22 11:54:27 +0200

    Ariketa2_v2 --> disenua polit polita

--- Interfazeen garapena/05 - Ariketa_2_Formula_V2/Ariketa_2/Form1.Designer.cs
+++ Interfazeen garapena/05 - Ariketa_2_Formula_V2/Ariketa_2/Form1.Designer.cs
@@ -38,47 +38,58 @@
             // lbZenbakiak
             // 
             lbZenbakiak.AutoSize = true;
+            lbZenbakiak.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbZenbakiak.ForeColor = SystemColors.HotTrack;
             lbZenbakiak.Location = new Point(127, 86);
             lbZenbakiak.Name = "lbZenbakiak";
-            lbZenbakiak.Size = new Size(85, 20);
+            lbZenbakiak.Size = new Size(158, 38);
             lbZenbakiak.TabIndex = 0;
-            lbZenbakiak.Text = "1. Zenabkia";
+            lbZenbakiak.Text = "1. Zenbakia";
             // 
             // tbZenbakiak
             // 
-            tbZenbakiak.Location = new Point(313, 83);
+            tbZenbakiak.Location = new Point(313, 97);
             tbZenbakiak.Name = "tbZenbakiak";
             tbZenbakiak.Size = new Size(340, 27);
             tbZenbakiak.TabIndex = 1;
             // 
             // btnHurrengoa
             // 
+            btnHurrengoa.BackColor = SystemColors.HotTrack;
+            btnHurrengoa.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnHurrengoa.ForeColor = SystemColors.Menu;
             btnHurrengoa.Location = new Point(127, 264);
             btnHurrengoa.Name = "btnHurrengoa";
             btnHurrengoa.Size = new Size(94, 29);
             btnHurrengoa.TabIndex = 2;
             btnHurrengoa.Text = "Hurrengoa";
-            btnHurrengoa.UseVisualStyleBackColor = true;
+            btnHurrengoa.UseVisualStyleBackColor = false;
             btnHurrengoa.Click += btnHurrengoa_Click;
             // 
             // btnGarbitu
             // 
+            btnGarbitu.BackColor = SystemColors.HotTrack;
+            btnGarbitu.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGarbitu.ForeColor = SystemColors.Menu;
             btnGarbitu.Location = new Point(346, 264);
             btnGarbitu.Name = "btnGarbitu";
             btnGarbitu.Size = new Size(94, 29);
             btnGarbitu.TabIndex = 3;
             btnGarbitu.Text = "Garbitu";
-            btnGarbitu.UseVisualStyleBackColor = true;
+            btnGarbitu.UseVisualStyleBackColor = false;
             btnGarbitu.Click += btnGarbitu_Click;
             // 
             // btnIrten
             // 
+            btnIrten.BackColor = SystemColors.HotTrack;
+            btnIrten.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnIrten.ForeColor = SystemColors.Menu;
             btnIrten.Location = new Point(559, 264);
             btnIrten.Name = "btnIrten";
             btnIrten.Size = new Size(94, 29);
             btnIrten.TabIndex = 4;
             btnIrten.Text = "Irten";
-            btnIrten.UseVisualStyleBackColor = true;
+            btnIrten.UseVisualStyleBackColor = false;
             btnIrten.Click += btnIrten_Click;
             // 
             // Form1
